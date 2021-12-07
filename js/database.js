7358bc860cce81fe061e4483a8fe33b3f4d1cc2b
GabrielAderaldo <gabriel_aderaldo@edu.unifor.br>
2021-12-06 22:52:15 -0300

    Componentizando o quem somos.

--- js/database.js
+++ js/database.js
@@ -17,7 +17,7 @@ class Aux{
             coordenadas:{ lat:-3.804844796524208, lng:-38.53404600286238, }
         }
 
-        /*
+        
 
       
 
@@ -31,7 +31,7 @@ class Aux{
             coordenadas:{ lat:-3.7691783232126874, lng: -38.48011135569974, }
         }
 
-        */
+        
 
         let AMAGER = {
             titulo:'AMAGER',
@@ -59,6 +59,7 @@ class Aux{
         vetorObj.push(acdg)
         vetorObj.push(AMAGER)
         vetorObj.push(AMPS)
+        vetorObj.push(exemplo)
 
         return vetorObj
 
@@ -143,6 +144,106 @@ class Aux{
         parceiros.push(unifor)
     
         return parceiros
+    }
+
+
+
+    databaseFuncionarios(){
+        let funcionarios = []
+
+        let monica = {
+            "nome":"Monica Aderaldo",
+            "funcao":"Presidente",
+            "foto":"https://drive.google.com/uc?export=view&id=1OQwwysD3bDy0cWLNoF6Ihfenykgve6QD",
+            "redesSocias":{
+                "fb":"",
+                "lk":"",
+                "tw":""
+            } 
+        }
+
+        let rodrigo = {
+            "nome":"Rodrigo Araujo",
+            "funcao":"Segundo tesolreiro",
+            "foto":"https://drive.google.com/uc?export=view&id=1z6GCUfkMa2AdsNIBkLgr-_BuSszbQEQx",
+            "redesSocias":{
+                "fb":"",
+                "lk":"",
+                "tw":""
+            } 
+        }
+
+        let fabio = {
+            "nome":"Fabio Marques",
+            "funcao":"Vice presidente",
+            "foto":"https://drive.google.com/uc?export=view&id=1NVfPDBhfRbsthJuUvt16q1weW5QpMP-w",
+            "redesSocias":{
+                "fb":"",
+                "lk":"",
+                "tw":""
+            } 
+        }
+
+        let paulo = {
+            "nome":"Paulo Da silva",
+            "funcao":"Segundo Secretario",
+            "foto":"https://drive.google.com/uc?export=view&id=1ohYs4rXqg0koK9VO6848cHVmekwZ8P7Z",
+            "redesSocias":{
+                "fb":"",
+                "lk":"",
+                "tw":""
+            } 
+        }
+
+        let levi = {
+            "nome":"Levi Vicente Dias",
+            "funcao":"Primeiro Tesoureiro",
+            "foto":"https://drive.google.com/uc?export=view&id=1mO8oLU_Dv-bVz-NP9Gie2uHQyOXdIQxs",
+            "redesSocias":{
+                "fb":"",
+                "lk":"",
+                "tw":""
+            } 
+        }
+
+        let dianna  = {
+            "nome":"Dianna dos Santos",
+            "funcao":"Primeiro Secretaria",
+            "foto":"https://drive.google.com/uc?export=view&id=1IZOukAO9MIyiaHLr5XIGOeQ4kAvpxkul",
+            "redesSocias":{
+                "fb":"",
+                "lk":"",
+                "tw":""
+            } 
+        }
+
+        let marlene  = {
+            "nome":"Marlene Vieira",
+            "funcao":"Assistente social",
+            "foto":"https://drive.google.com/uc?export=view&id=1h_5ZKF60X35ujyUL15cB1ZNS220OTg8O",
+            "redesSocias":{
+                "fb":"",
+                "lk":"",
+                "tw":""
+            } 
+        }
+
+
+        funcionarios.push(monica)
+        funcionarios.push(fabio)
+        funcionarios.push(dianna)
+        funcionarios.push(paulo)
+        funcionarios.push(levi)
+        funcionarios.push(rodrigo)
+        funcionarios.push(marlene)
+        
+
+        return funcionarios
+    }
+
+
+    databaseChamadaPrincipal(){
+        //TODO:FAZER QUANDO TIVER SACO
     }
 
 
